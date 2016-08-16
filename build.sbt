40b8506f74b412be894bd2eb65c6692133c8b399
Nelson Iván González <nigm2005@gmail.com>
2016-08-16 00:30:04 -0500

    Some migrations

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "tonali-api"
 version := "1.0"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.36"
+libraryDependencies += "com.typesafe.play" % "play-slick" % "1.1.0"
+libraryDependencies += "com.typesafe.play" % "play-slick-evolutions" % "1.1.0"
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,39 @@
+# --- !Ups
+
+CREATE TABLE app (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(100) NOT NULL,
+	version INT NOT NULL,
+	password CHAR(60) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE user (
+	id INT NOT NULL AUTO_INCREMENT,
+	email TEXT NOT NULL,
+	password CHAR(60) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE token (
+	id INT NOT NULL AUTO_INCREMENT,
+	user INT NOT NULL,
+	token CHAR(36) NOT NULL,
+	expires TIMESTAMP NOT NULL,
+	context INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (user) REFERENCES user(id),
+	FOREIGN KEY (context) REFERENCES app(id)
+);
+
+# web_password
+INSERT INTO app (name, version, password) VALUES ('web', 1, '$2a$04$Y3egpcUwPGkJbSNoTfjb9OSBHIvCL7PaJj4U2kITAmPOLSk5O4x2.');
+
+# password
+INSERT INTO user (email, password) VALUES ('admin@foo.com', '$2a$04$GLM2NIxoELWX6zaUQuUxL.r0OvVP.lkXUF5uCxv9Buoa1YH/Uv1PS');
+
+# --- !Downs
+
+DROP TABLE app;
+DROP TABLE user;
+DROP TABLE token;
